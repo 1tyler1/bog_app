976ae319630a16c7fc64d1b03bb715522add212b
1tyler1 <mya12ga@yahoo.com>
2018-04-26 12:17:22 -0400

    updating classwork

--- db/seeds.js
+++ db/seeds.js
@@ -1,1 +1,15 @@
 require('dotenv').config()
+const mongoose = require('mongoose')
+const { Creature } = require('./schema')
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+
+const saved = async () => {
+    await Creature.remove()
+    const luke = new Creature
+}
+
+saved()
